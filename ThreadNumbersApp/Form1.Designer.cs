72c4269ff7f77e47a5594e03c94e648b86e549d5
PavelSadykov <2439747@gmail.com>
2023-12-19 00:02:58 +0500

    Добавьте файлы проекта.

---
+++ ThreadNumbersApp/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ThreadNumbersApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.buttonContinue = new System.Windows.Forms.Button();
+            this.buttonIdle2 = new System.Windows.Forms.Button();
+            this.buttonContinue2 = new System.Windows.Forms.Button();
+            this.buttonIdle = new System.Windows.Forms.Button();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(561, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(561, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 394);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(668, 35);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 394);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // buttonContinue
+            // 
+            this.buttonContinue.Location = new System.Drawing.Point(157, 96);
+            this.buttonContinue.Name = "buttonContinue";
+            this.buttonContinue.Size = new System.Drawing.Size(96, 23);
+            this.buttonContinue.TabIndex = 7;
+            this.buttonContinue.Text = "Возобновить";
+            this.buttonContinue.UseVisualStyleBackColor = true;
+            this.buttonContinue.Click += new System.EventHandler(this.buttonContinue_Click);
+            // 
+            // buttonIdle2
+            // 
+            this.buttonIdle2.Location = new System.Drawing.Point(561, 67);
+            this.buttonIdle2.Name = "buttonIdle2";
+            this.buttonIdle2.Size = new System.Drawing.Size(101, 23);
+            this.buttonIdle2.TabIndex = 8;
+            this.buttonIdle2.Text = "Приостановить";
+            this.buttonIdle2.UseVisualStyleBackColor = true;
+            this.buttonIdle2.Click += new System.EventHandler(this.buttonIdle2_Click);
+            // 
+            // buttonContinue2
+            // 
+            this.buttonContinue2.Location = new System.Drawing.Point(561, 96);
+            this.buttonContinue2.Name = "buttonContinue2";
+            this.buttonContinue2.Size = new System.Drawing.Size(101, 23);
+            this.buttonContinue2.TabIndex = 9;
+            this.buttonContinue2.Text = "Возобновить";
+            this.buttonContinue2.UseVisualStyleBackColor = true;
+            this.buttonContinue2.Click += new System.EventHandler(this.buttonContinue2_Click);
+            // 
+            // buttonIdle
+            // 
+            this.buttonIdle.Location = new System.Drawing.Point(157, 67);
+            this.buttonIdle.Name = "buttonIdle";
+            this.buttonIdle.Size = new System.Drawing.Size(96, 23);
+            this.buttonIdle.TabIndex = 10;
+            this.buttonIdle.Text = "Приостановить";
+            this.buttonIdle.UseVisualStyleBackColor = true;
+            this.buttonIdle.Click += new System.EventHandler(this.buttonIdle_Click);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(327, 223);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(141, 32);
+            this.buttonRestart.TabIndex = 11;
+            this.buttonRestart.Text = "Restart";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestartPrime_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Простые числа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Введите конец диапазона";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(324, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Введите начало диапазона";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(327, 96);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(141, 20);
+            this.txtEnd.TabIndex = 14;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(327, 40);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(141, 20);
+            this.txtStart.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(665, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = " Числа Фибоначчи";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.buttonIdle);
+            this.Controls.Add(this.buttonContinue2);
+            this.Controls.Add(this.buttonIdle2);
+            this.Controls.Add(this.buttonContinue);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button buttonContinue;
+        private System.Windows.Forms.Button buttonIdle2;
+        private System.Windows.Forms.Button buttonContinue2;
+        private System.Windows.Forms.Button buttonIdle;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ThreadNumbersApp/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+
+namespace ThreadNumbersApp
+{
+    public partial class Form1 : Form
+    {
+      
+        Thread primeThread;// поток для простых чисел
+        Thread fbThread;// поток для  чисел Фибоначчи
+        private CancellationTokenSource tokenSource1;//флаг для остановки потока простых чисел
+        private CancellationTokenSource tokenSource2;//флаг для остановки потока чисел Фибоначчи
+        private bool isGenerating;//лог переменная процесса генерации потока простых чисел
+        private bool isGeneratingFibonacci;//лог переменная процесса генерации потока  чисел Фибоначчи
+        List<int>primeNumbers = new List<int>();// список для простых чисел
+        List<int> fbNumbers = new List<int>();// список для чисел Фб
+        public Form1()
+        {
+
+            InitializeComponent();
+            tokenSource1 = new CancellationTokenSource();
+            tokenSource2 = new CancellationTokenSource();   
+        }
+
+       //*********ПРОСТЫЕ ЧИСЛА **************
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tokenSource1 = new CancellationTokenSource();
+            if (!isGenerating)
+            {
+                // если процесс не запущен, метод получает нач и конечное значения и начинает генерацию в потоке
+                // если данные не введены , диапазон по умолчанию от 2 до 500
+                int startNumber = string.IsNullOrEmpty(txtStart.Text) ? 2 : int.Parse(txtStart.Text);
+                int endNumber = string.IsNullOrEmpty(txtEnd.Text) ? 500 : int.Parse(txtEnd.Text);
+
+                isGenerating = true;
+
+
+                 GeneratePrimeNumbers(startNumber, endNumber);//отправляем в обработку для получения списка простых чисел
+
+                primeThread = new Thread(() => GenerateAndShowNumbers(tokenSource1, listBox1, primeNumbers));
+                primeThread.Start();
+            }
+            
+        }
+
+        //метод генерации простых чисел
+        private void GeneratePrimeNumbers(int start, int end)
+        {
+            for (int i = start; i <= end && isGenerating; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primeNumbers.Add(i);//добавляем в список простые числа
+                }
+            }
+            isGenerating = false;
+        }
+
+
+        //метод проверки числа на простое 
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)// метод проверки деления на все числа от 2 до квадр корня из числа
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+
+        //*************ЧИСЛА ФИБОНАЧЧИ*********
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            tokenSource2 = new CancellationTokenSource();
+            if (!isGeneratingFibonacci)
+            {
+                // если процесс не запущен, метод получает нач и конечное значения и начинает генерацию в потоке
+                int startNumber = string.IsNullOrEmpty(txtStart.Text) ? 2 : int.Parse(txtStart.Text);
+                int endNumber = string.IsNullOrEmpty(txtEnd.Text) ? 1000000 : int.Parse(txtEnd.Text);
+
+                isGeneratingFibonacci = true;
+
+
+                GenerateFibonacciInRange(startNumber, endNumber);//отправляем в обработку для получения списка чисел ФБ
+
+
+                fbThread = new Thread(() => GenerateAndShowNumbers(tokenSource2, listBox2, fbNumbers));
+                fbThread.Start();
+            }
+           
+        }
+
+        //метод генерации списка  чисел Фибоначчи
+
+        private void GenerateFibonacciInRange(int start, int end)
+        {
+            List<int> fibonacciInRange = GetFibonacciInRange(start, end);// сохранение списка в переменную 
+
+            this.Invoke((MethodInvoker)delegate //Инвок для безопасного обновления из другого потока
+            {
+                foreach (int number in fibonacciInRange)// перебираем
+                {
+                  fbNumbers.Add(number);//выводим числа Фиб. из списка  в лист бокс
+                   
+                }
+            });
+
+            isGeneratingFibonacci = false;// флаг завершения процесса генерации чисел
+          
+        }
+
+        private List<int> GetFibonacciInRange(int start, int end)
+        {
+            List<int> fibonacciInRange = new List<int>();//инициализируем пустой список для хранения чисел Фиб.
+
+            int a = 0, b = 1;//начальные значения
+            int temp = 0;
+
+            while (temp <= end)
+            {
+                temp = a + b;// вычисление ччисел Фб.
+                a = b;
+                b = temp;
+
+                if (temp >= start && temp <= end)
+                {
+                    fibonacciInRange.Add(temp);// добавляем  в список
+                }
+            }
+
+            return fibonacciInRange;// возвращаем список с числами в диапазоне
+        }
+
+        //выводим на форму поток
+        private void GenerateAndShowNumbers(CancellationTokenSource tokenSource,   ListBox listBox, List<int>numbers)
+        {
+            try
+            {
+                foreach (int number in numbers)
+                {
+                    if (tokenSource.IsCancellationRequested)//если поток остановлен
+                        break;
+
+                    listBox.BeginInvoke((MethodInvoker)delegate {
+                        listBox.Items.Add(number);//вывод на форму 
+                    });
+
+                    Thread.Sleep(200);//задержка для вывода на форму (для удобства восприятия)
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Restart");
+            }
+           
+           
+
+        }
+      
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           tokenSource1.Cancel();//поток остановлен
+        }
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            tokenSource2.Cancel();//поток остановлен
+        }
+        [Obsolete]
+        private void buttonIdle_Click(object sender, EventArgs e)
+        {
+            if(primeThread!=null && primeThread.IsAlive)
+            {//поток выполняется
+                primeThread.Suspend();//приостанавливаем
+            }
+            //поток не выполняется ( завершен или не был создан)
+        }
+        [Obsolete]
+        private void buttonContinue_Click(object sender, EventArgs e)
+        {
+            if(primeThread != null && primeThread.ThreadState == ThreadState.Suspended)
+            {//поток выполняется и его статус - приостановлен
+                primeThread.Resume();//возобновляем
+            }
+        }
+        [Obsolete]
+        private void buttonIdle2_Click(object sender, EventArgs e)
+        {
+            if (fbThread != null && fbThread.IsAlive)
+            {
+                fbThread.Suspend();
+            }
+        }
+        [Obsolete]
+        private void buttonContinue2_Click(object sender, EventArgs e)
+        {
+            if (fbThread != null && fbThread.ThreadState == ThreadState.Suspended)
+            {
+                fbThread.Resume();
+            }
+        }
+
+        private void buttonRestartPrime_Click(object sender, EventArgs e)
+        {
+            tokenSource1.Cancel();//поток остановлен
+            tokenSource2.Cancel();//поток остановлен
+            isGenerating = false;//флаги в исходное состояние
+            isGeneratingFibonacci = false;
+            listBox1.Items.Clear();//очищаем элементы на форме от значений
+            listBox2.Items.Clear();
+            txtEnd.Clear();
+            txtStart.Clear();
+            primeNumbers.Clear();//очищаем списки от значений
+            fbNumbers.Clear();
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
